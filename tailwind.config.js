67842f6432e65320f1c42d2b9ac1961604fa5bf5
Al Amin <alamin.mistrisolutions@gmail.com>
2021-07-14 10:24:19 +0600

    sticky

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: [],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
